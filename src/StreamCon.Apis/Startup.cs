19e82793b3da270bbb37e8d6c53eccf7f56edeb5
David Crook <dacrook@microsoft.com>
2020-01-23 15:44:22 -0500

    lots of initial stuff

---
+++ src/StreamCon.Apis/Startup.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StreamCon.Core.Data;
+
+namespace StreamCon.Apis
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<StreamConContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DbConnString"));
+            });
+
+            services.AddScoped<IStreamConRepository, StreamConRepository>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Default",
+                    "{controller}/{action}/{id?}");
+            });
+        }
+    }
+}
---
+++ src/StreamCon.UI/Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamCon.UI.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Index";
+            ViewBag.ApiEndpoint = "Not Configured";
+            return View();
+        }
+    }
+}
